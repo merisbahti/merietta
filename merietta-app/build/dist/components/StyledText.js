1d525573c918dec6420ec628a757281784bb2e66
Meris Bahti <meris@mbp>
2019-01-24 12:43:37 +0100

    Ignore build

--- merietta-app/build/dist/components/StyledText.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { Text } from 'react-native';
-export class MonoText extends React.PureComponent {
-    render() {
-        return React.createElement(Text, Object.assign({}, this.props, { style: [this.props.style, { fontFamily: 'space-mono' }] }));
-    }
-}
-//# sourceMappingURL=StyledText.js.map
